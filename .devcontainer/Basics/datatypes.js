d1a830a876019f7a7d9f88d38f64b418b3e2394b
Rahul Singh <mailrahulsingh05@gmail.com>
2026-05-25 19:07:53 +0000

    datatypes

---
+++ .devcontainer/Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict" ;//treat all js code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+console.log(3+3) ; //code readability should be high
+console.log("rahul")
+
+let name="rahul"
+let age=20
+let isloggedIn=false
+
+
+//number=>2 to power 53
+//bigInt
+//string=>""
+//boolean=>true/false
+//null =>standalone value
+//undefined => abhi value assign nhi hui h --let age;
+//symbol =>unique
+
+//object
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
+
+
