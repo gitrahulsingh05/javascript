86eaa84d48abf7c1af52c7dd426675471ece8590
Rahul Singh <mailrahulsingh05@gmail.com>
2026-05-25 19:40:40 +0000

    operation

---
+++ .devcontainer/Basics/operation.js
@@ -0,0 +1,41 @@
+// let value=3
+// let negvalue=-value
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2%3);
+
+
+let str1="hello"
+let str2=" rahul"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);  //12
+// console.log(1+"2");  //12
+// console.log("1"+2+2);  //122
+// console.log(2+2+"1");  //41
+
+// console.log(+true);  //1
+// console.log(true+); //eroor
+
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+
+
+// let gameCounter=100
+// gameCounter++;
+// console.log(gameCounter); //101
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);//101
+
+
+
+
+
+
+
+
