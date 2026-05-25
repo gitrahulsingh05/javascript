a06a8ff5d2422016b5d5116d6c8e8bd91aed9aa5
Rahul Singh <mailrahulsingh05@gmail.com>
2026-05-25 18:50:16 +0000

    variables

---
+++ .devcontainer/Basics/variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="rahul@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+
+// accountId=2
+accountEmail="hs@hc.com"
+accountpassword="212121"
+accountCity="Bangluru"
+console.log(accountId);
+/* 
+prefered not to use var 
+beacuse issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState])
