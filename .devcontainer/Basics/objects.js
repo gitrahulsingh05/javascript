67a0d660ce09d400484c461fff424c5f0236cb21
Rahul Singh <mailrahulsingh05@gmail.com>
2026-06-08 11:37:57 +0000

    objects

--- .devcontainer/Basics/objects.js
+++ .devcontainer/Basics/objects.js
@@ -1,6 +1,58 @@
 //singleton
 
-const JsUser={
-name:"rahul"
 
+//object litrals
+// const JsUser={
+// name:"rahul",
+// age:20,
+// email:"rahul@email.com",
+// isloggeding:false,
+
+// }
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);  //second way to get objects 
+
+
+// JsUser.greeting=function(){
+//     console.log('hello js user');
+    
+// }
+// console.log(JsUser.greeting());
+
+
+
+// const tinderuser = new Object()
+
+//  tinderuser.id="123abc"
+//  tinderuser.name="sammy"
+//  tinderuser.isLoggedin=false;
+
+//     // console.log(tinderuser);
+//     const regularuser={
+//     email:"rahul@rmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname:"rahul"
+//         }
+//     }
+// }
+// console.log(regularuser.email.fullname);
+
+// const obj1={1:"a",2:"b"}
+// const obj2={3:"a",4:"b"}
+
+// // const obj3={obj1 ,obj2}
+// const obj3=Object.assign(obj1,obj2)
+// console.log(obj3);
+
+
+
+const course={
+    coursename:"js rahul",
+    price:"999",
+    courseinstructor:"hitesh"
 }
+
+console.log(courseinstructor);
+
+
